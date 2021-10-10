1211ecec08015e87f9f97a23cf5242d907d644e4
Leo Yang <leo19941227@gmail.com>
2021-10-10 07:40:11 -0400

    specify GPU num

--- twcc.sh
+++ twcc.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -x
+set -e
 
+if [ $# != 1 ]; then
+    echo Please specify GPU num.
+    exit 1
+fi
+
+gpu=$1
 launch_name="leo-$(date +%s)"
 
 # launch machine
-launch_id=$(twccli mk ccs -wait -json -gpu 1 -itype "Custom Image" -img "pytorch-21.06-py3:leo19941227-v1.0" -n $launch_name | jq -r ".id")
+launch_id=$(twccli mk ccs -wait -json -gpu $gpu -itype "Custom Image" -img "pytorch-21.06-py3:leo19941227-v1.0" -n $launch_name | jq -r ".id")
 ssh_info=$(twccli ls ccs -s $launch_id -gssh)
 ssh -o StrictHostKeyChecking=no -t $ssh_info "cd /work/leo19941227; tmux new -s $launch_name"
 
