5da2e00178345e1e45001de5d04e05e592ab242b
Leo Yang <leo19941227@gmail.com>
2021-12-21 16:36:16 +0800

    resume tmux

---
+++ resume.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+set -e
+
+if [ $# != 1 ]; then
+    echo $0 launch_id
+    exit 2
+fi
+
+# launch machine
+launch_id="$1"
+ssh_info=$(twccli ls ccs -s $launch_id -gssh)
+ssh -o StrictHostKeyChecking=no -t $ssh_info "if [ \"\$(tmux ls | wc -l)\" == 1 ]; then tmux a; else exit 0; fi"
+
+# stop machine
+twccli rm ccs -f -s $launch_id
+
